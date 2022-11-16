235e33aadcf924fa60c51a751a6487d1e9c434f6
KyleOlhausen <olhausen.kyle43@gmail.com>
2022-11-16 15:56:39 -0600

    added index.html, main.js, styles.css

---
+++ main.js
@@ -0,0 +1,23 @@
+
+
+
+
+function Player(name)
+{
+    this.name = name;
+}
+
+
+const Gameboard = (() => {
+    let gameboard = [
+        [x, x, x],
+        [x, x, x],
+        [x, x, x]
+    ];
+})();
+
+
+const Game = (() => {
+
+})();
+
